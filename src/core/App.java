d8b0e6291fd8ee2a5c705cc9523e605287459fe6
Alexander <alexander@Alexanders-MacBook-Pro.local>
2017-04-05 23:17:28 -0700

    Initital commit

---
+++ src/core/App.java
@@ -0,0 +1,41 @@
+package core;
+
+import java.text.DecimalFormat;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+@Path("/xml")
+
+public class App {
+
+	@GET
+	@Path("/imperial")
+	@Produces("application/xml")
+	public String CalcImperial(@DefaultValue("1") @QueryParam("height") int h, // h
+																				// in
+																				// inches
+			@DefaultValue("1") @QueryParam("weight") int w) { // w in pounds
+		DecimalFormat df = new DecimalFormat("####0.0");
+		Double bmi = (double) w / (h * h) * 703; // w / Math.pow(h, 2) * 703
+		String result = df.format(bmi).toString();
+		return "<calc unit=\"imperial\">" + "<height>" + h + "</height>" + "<weight>" + w + "</weight>" + "<bmi>"
+				+ result + "</bmi>" + "</calc>";
+	}
+
+	@GET
+	@Path("/metric")
+	@Produces("application/xml")
+	public String CalcMetric(@DefaultValue("1") @QueryParam("height") int h, // h
+																				// in
+																				// centimeter
+			@DefaultValue("1") @QueryParam("weight") int w) { // w in kilogram
+		DecimalFormat df = new DecimalFormat("####0.0");
+		Double bmi = (double) (((w / (h * h)) * 10000));
+		String result = df.format(bmi).toString();
+		return "<calc unit=\"metric\">" + "<height>" + h + "</height>" + "<weight>" + w + "</weight>" + "<bmi>" + result
+				+ "</bmi>" + "</calc>";
+	}
+}
